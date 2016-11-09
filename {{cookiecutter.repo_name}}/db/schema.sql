33a17e737892c87e0af402c1af1aeaba1d7c7f00
jhillgithub <jhillgithub@users.noreply.github.com>
2016-11-08 20:58:14 -0600

    added additional templating inside files

---
+++ {{cookiecutter.repo_name}}/db/schema.sql
@@ -0,0 +1,4 @@
+### Schema
+
+CREATE DATABASE {{cookiecutter.db_name}};
+USE {{cookiecutter.db_name}};
---
+++ {{cookiecutter.repo_name}}/models/{{cookiecutter.model_name}}.js
@@ -0,0 +1,11 @@
+/*
+Here is where you setup a model for how to interface with the database.
+*/
+
+var orm = require('../config/orm.js');
+
+var {{cookiecutter.model_name}} = {
+	
+};
+
+module.exports = {{cookiecutter.model_name}};
